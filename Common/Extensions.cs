c3c4869e0330ebe4b503cf43524d15c1b6599f3e
morpe <morphboy001@hotmail.com>
2025-07-07 22:17:39 +1000

    Add project files.

---
+++ Common/Extensions.cs
@@ -0,0 +1,95 @@
+﻿using MonoMod.Cil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil.Cil;
+
+namespace RightClickFromInventory.Common
+{
+    public static class Extensions
+    {
+        // Credits to https://github.com/absoluteAquarian/SerousCommonLib/blob/bc7b9bca6eff4fc12bfa09b54dc4b4d2ef081010/src/API/Helpers/ILHelper.cs ... I hope this is no issue!
+        /// <summary>
+		/// Updates the instruction offsets within <paramref name="c"/>
+		/// </summary>
+		/// <param name="c">The cursor</param>
+        public static void UpdateInstructionOffsets(this ILCursor c)
+        {
+            var instrs = c.Instrs;
+            int curOffset = 0;
+
+            static Instruction[] ConvertToInstructions(ILLabel[] labels)
+            {
+                Instruction[] ret = new Instruction[labels.Length];
+
+                for (int i = 0; i < labels.Length; i++)
+                    ret[i] = labels[i].Target;
+
+                return ret;
+            }
+
+            foreach (var ins in instrs)
+            {
+                ins.Offset = curOffset;
+
+                if (ins.OpCode != OpCodes.Switch)
+                    curOffset += ins.GetSize();
+                else
+                {
+                    //'switch' opcodes don't like having the operand as an ILLabel[] when calling GetSize()
+                    //thus, this is required to even let the mod compile
+
+                    Instruction copy = Instruction.Create(ins.OpCode, ConvertToInstructions((ILLabel[])ins.Operand));
+                    curOffset += copy.GetSize();
+                }
+            }
+        }
+
+        public static void RedirectBranchOperands(this ILCursor cursor, ILLabel oldLabel, ILLabel newLabel, params ILLabel[] ignoreLabels) => RedirectBranchOperands(cursor, oldLabel.Target, newLabel.Target, ignoreLabels.Select(illabel => illabel.Target).ToArray());
+        public static void RedirectBranchOperands(this ILCursor cursor, Instruction oldInstruction, Instruction newInstruction, params Instruction[] ignoreInstructions)
+        {
+            int cursorIndex = cursor.Index;
+
+            cursor.Goto(newInstruction);
+            ILLabel newLabel = cursor.MarkLabel();
+
+            cursor.Index = cursorIndex;
+
+            for (int i = 0; i < cursor.Instrs.Count; i++)
+            {
+                Instruction ins = cursor.Instrs[i];
+
+                if (ignoreInstructions.Any(ignore => ignore == ins))
+                {
+                    continue;
+                }
+
+                if (ins.Operand is ILLabel label)
+                {
+                    if (label.Target == oldInstruction)
+                    {
+                        cursor.Instrs[i].Operand = newLabel;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Finds instruction relative to the current cursor index. <paramref name="direction"/> 0 is same as <see cref="ILCursor.Next"/>, -1 is same as <see cref="ILCursor.Prev"/>.
+        /// </summary>
+        /// <param name="cursor"></param>
+        /// <param name="direction"></param>
+        /// <returns></returns>
+        public static Instruction Relative(this ILCursor cursor, int direction)
+        {
+            return cursor.Instrs[cursor.Index + direction];
+        }
+
+        public static ILLabel MarkLabel(this ILCursor cursor, int direction)
+        {
+            return cursor.MarkLabel(cursor.Instrs[cursor.Index + direction]);
+        }
+    }
+}
---
+++ RightClickFromInventory.cs
@@ -0,0 +1,293 @@
+using MonoMod.Cil;
+using RightClickFromInventory.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.UI;
+using Terraria.GameInput;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+
+namespace RightClickFromInventory
+{
+	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
+	public class RightClickFromInventory : Mod
+	{
+        public static HashSet<int> ItemHasAltFunctionUse { get; } =
+        [
+            ItemID.RubblemakerSmall,
+            ItemID.RubblemakerMedium,
+            ItemID.RubblemakerLarge,
+            ItemID.EncumberingStone,
+            ItemID.UncumberingStone,
+            ItemID.ClosedVoidBag,
+            ItemID.VoidLens,
+            ItemID.DontHurtComboBook,
+            ItemID.DontHurtComboBookInactive,
+            ItemID.DontHurtCrittersBook,
+            ItemID.DontHurtCrittersBookInactive,
+            ItemID.DontHurtNatureBook,
+            ItemID.DontHurtNatureBookInactive,
+            ItemID.Shellphone,
+            ItemID.ShellphoneOcean,
+            ItemID.ShellphoneHell,
+            ItemID.ShellphoneSpawn,
+            ItemID.PortalGun,
+            ItemID.MonkStaffT3,
+            ItemID.DrumStick,
+            ItemID.BookStaff,
+            ItemID.BouncingShield,
+            ItemID.DD2SquireDemonSword,
+            ItemID.MulticolorWrench,
+            ItemID.WireKite
+        ];
+        public HashSet<string> DisabledItems { get; } =
+        [
+            "Psychedelic_Prism/PsychedelicPrism"
+        ];
+
+        public static bool HasAltUse(Item item)
+        {
+            if (ItemHasAltFunctionUse.Contains(item.type) || ProjectileID.Sets.TurretFeature[item.shoot] || ProjectileID.Sets.MinionTargettingFeature[item.shoot])
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override void Load()
+        {
+            IL_Player.ItemCheck_ManageRightClickFeatures += (context) =>
+            {
+                var cursor = new ILCursor(context);
+
+                if (cursor.TryGotoNext(MoveType.Before,
+                    i => i.MatchLdarg(0),
+                    i => i.MatchLdfld<Player>("selectedItem"),
+                    i => i.MatchLdcI4(out _),
+                    i => i.MatchBeq(out _)
+                    ))
+                {
+                    cursor.RemoveRange(4);
+                }
+
+                if (cursor.TryGotoNext(MoveType.Before,
+                    i => i.MatchLdarg(0),
+                    i => i.MatchLdfld<Player>("inventory"),
+                    i => i.MatchLdarg(0),
+                    i => i.MatchLdfld<Player>("selectedItem"),
+                    i => i.MatchLdelemRef(),
+                    i => i.MatchLdloc(5),
+                    i => i.MatchCallvirt<Item>("ChangeItemType")
+                    ))
+                {
+                    cursor.EmitLdarg(0);
+                    cursor.EmitLdloc(5);
+                    cursor.EmitDelegate((Player player, int changeTo) =>
+                    {
+                        if (RightClickFromInventoryConfig.Instance.enabled && player.selectedItem == 58)
+                        {
+                            Main.mouseItem.ChangeItemType(changeTo);
+                        }
+                    });
+                }
+
+                var label = cursor.DefineLabel();
+                if (cursor.TryGotoNext(MoveType.Before,
+                    i => i.MatchLdloc(1),
+                    i => i.MatchBrfalse(out label),
+
+                    i => i.MatchLdarg(0),
+                    i => i.MatchLdfld<Player>("altFunctionUse"),
+                    i => i.MatchBrtrue(out _),
+
+                    i => i.MatchLdarg(0),
+                    i => i.MatchLdfld<Player>("inventory"),
+                    i => i.MatchLdarg(0),
+                    i => i.MatchLdfld<Player>("selectedItem"),
+                    i => i.MatchLdelemRef(),
+                    i => i.MatchLdarg(0),
+                    i => i.MatchCall("Terraria.ModLoader.ItemLoader", "AltFunctionUse"),
+                    i => i.MatchBrfalse(out _)
+                    ))
+                {
+                    var firstOriginalLabel = cursor.DefineLabel();
+                    cursor.EmitLdarg0();
+                    var firstNewLabel = cursor.MarkLabel(cursor.Prev);
+                    cursor.EmitDelegate((Player player) =>
+                    {
+                        return RightClickFromInventoryConfig.Instance.enabled && player.selectedItem == 58 && (player.HeldItem.ModItem == null || !DisabledItems.Contains(player.HeldItem.ModItem.FullName));
+                    });
+                    var ignore1 = cursor.EmitBrfalse(firstOriginalLabel).Prev; // Goto regular instructions 
+
+                    cursor.EmitLdarg0();
+                    cursor.EmitLdloc(1);
+                    cursor.EmitDelegate((Player player, bool someFlag) =>
+                    {
+                        var item = player.inventory[player.selectedItem];
+                        bool altFunctionUse = ItemLoader.AltFunctionUse(item, player);
+                        if (altFunctionUse)
+                        {
+                            ItemHasAltFunctionUse.Add(item.type);
+                        }
+                        if (someFlag && player.altFunctionUse == 0 && altFunctionUse)
+                        {
+                            player.altFunctionUse = 1;
+                            player.controlUseItem = true;
+                        }
+                    });
+
+                    cursor.EmitBr(label); // Skip regular instructions
+
+                    cursor.MarkLabel(firstOriginalLabel);
+                    cursor.RedirectBranchOperands(firstOriginalLabel.Target, firstNewLabel.Target, ignore1);
+
+                    cursor.UpdateInstructionOffsets();
+                }
+
+                //MonoModHooks.DumpIL(ModContent.GetInstance<RightClickFromInventory>(), context);
+            };
+
+            IL_Player.dropItemCheck += (context) =>
+            {
+                var cursor = new ILCursor(context);
+                if (cursor.TryGotoNext(MoveType.Before,
+                        i => i.MatchLdarg(0),
+                        i => i.MatchCall<Player>("DropSelectedItem")
+                        ))
+                {
+                    var branch = (ILLabel)cursor.Prev.Operand;
+
+                    cursor.EmitLdarg(0);
+                    cursor.EmitDelegate((Player player) =>
+                    {
+                        if (!ModContent.GetInstance<RightClickFromInventoryConfig>().enabled || player.controlThrow || (Main.mouseRight && !player.mouseInterface && Main.mouseRightRelease && player.controlTorch && Main.playerInventory))
+                        {
+                            return true;
+                        }
+
+                        return !HasAltUse(player.inventory[player.selectedItem]);
+                    });
+                    cursor.EmitBrfalse(branch);
+                }
+            };
+
+            IL_WiresUI.WiresRadial.FlowerUpdate += (context) =>
+            {
+                var cursor = new ILCursor(context);
+                if (cursor.TryGotoNext(MoveType.Before,
+                    i => i.MatchLdsfld("Terraria.Main", "mouseItem"),
+                    i => i.MatchLdfld("Terraria.Item", "type"),
+                    i => i.MatchLdcI4(out _),
+                    i => i.MatchBle(out _)
+                    ))
+                {
+                    cursor.RemoveRange(3);
+                    cursor.EmitDelegate(() =>
+                    {
+                        return RightClickFromInventoryConfig.Instance.enabled || Main.mouseItem.type <= ItemID.None;
+                    });
+                    cursor.Next.OpCode = Mono.Cecil.Cil.OpCodes.Brtrue;
+                }
+            };
+
+            IL_WiresUI.WiresRadial.LineUpdate += (context) =>
+            {
+                var cursor = new ILCursor(context);
+                if (cursor.TryGotoNext(MoveType.Before,
+                    i => i.MatchLdsfld("Terraria.Main", "mouseItem"),
+                    i => i.MatchLdfld("Terraria.Item", "type"),
+                    i => i.MatchLdcI4(out _),
+                    i => i.MatchBle(out _)
+                    ))
+                {
+                    cursor.RemoveRange(3);
+                    cursor.EmitDelegate(() =>
+                    {
+                        return RightClickFromInventoryConfig.Instance.enabled || Main.mouseItem.type <= ItemID.None;
+                    });
+                    cursor.Next.OpCode = Mono.Cecil.Cil.OpCodes.Brtrue;
+                }
+            };
+        }
+	}
+
+    public class RightClickFromInventoryPlayer : ModPlayer
+    {
+        public override void PostItemCheck()
+        {
+            // Main.NewText(string.Join(", ", RightClickFromInventory.ItemHasAltFunctionUse.Select(id => ContentSamples.ItemsByType[id].Name)));
+            //var t = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(asm => asm.GetName().Name == "CalamityMod");
+            //if (t != null)
+            //{
+            //    var cmodplayer = t.GetType("CalamityMod.CalPlayer.CalamityPlayer");
+            //    if (cmodplayer != null)
+            //    {
+
+                    
+            //    }
+            //}
+
+            //var field = typeof(Player).GetField("modPlayers", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            //if (field != null)
+            //{
+            //    var value = (ModPlayer[])field.GetValue(Main.LocalPlayer);
+            //    var calamityModPlayer = value.FirstOrDefault(i => i.Name == "CalamityPlayer");
+            //    if (calamityModPlayer != null)
+            //    {
+            //        var field2 = calamityModPlayer.GetType().GetField("mouseRight", System.Reflection.BindingFlags.Public | BindingFlags.Instance);
+            //        Main.NewText(calamityModPlayer.GetType().Name);
+            //        if (field2 != null)
+            //        {
+            //            var value2 = (bool)field2.GetValue(calamityModPlayer);
+            //            Main.NewText(value2);
+            //        }
+            //        else
+            //        {
+            //            Main.NewText("nuh");
+            //        }
+            //    }
+            //}
+
+            if (RightClickFromInventoryConfig.Instance.enabled && RightClickFromInventoryConfig.Instance.showHoldThrowText && Player.whoAmI == Main.myPlayer && Main.mouseItem != null)
+            {
+                if (Main.mouseRight && !Player.mouseInterface && Main.playerInventory && RightClickFromInventory.HasAltUse(Main.mouseItem))
+                {
+                    string input = "UNBOUND";
+                    if (PlayerInput.CurrentProfile.InputModes[InputMode.Keyboard].KeyStatus["SmartSelect"].Count > 0)
+                    {
+                        input = string.Join("/", PlayerInput.CurrentProfile.InputModes[InputMode.Keyboard].KeyStatus["SmartSelect"]);
+                    }
+                    Main.instance.MouseText(Language.GetTextValue("Mods.RightClickFromInventory.HoldThrow", input));
+                }
+            }
+
+            if (Main.LocalPlayer.HeldItem.ModItem != null)
+            {
+                Main.NewText(Main.LocalPlayer.HeldItem.ModItem.FullName);
+            }
+        }
+    }
+
+    public class RightClickFromInventoryConfig : ModConfig
+    {
+        public static RightClickFromInventoryConfig Instance;
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [DefaultValue(true)]
+        public bool enabled;
+
+        [DefaultValue(true)]
+        public bool showHoldThrowText;
+
+    }
+}
